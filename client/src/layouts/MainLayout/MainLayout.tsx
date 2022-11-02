5aa05e42f4fc2ed904db70ce9a4a0254ab5ea49f
alex.n.kior <alex.n.kior@gmail.com>
2022-11-02 22:02:43 +0300

    Add sass + project structure on client

---
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import style from './MainLayout.module.scss';
+
+const MainLayout: React.FC = function MainLayout(params:any) {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default MainLayout;
