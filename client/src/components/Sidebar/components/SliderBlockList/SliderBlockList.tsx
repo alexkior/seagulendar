15317f1dd9a268b566ed063094e020b7db8eb0bd
alex.n.kior <alex.n.kior@gmail.com>
2022-11-07 02:43:51 +0300

    Fix layout

---
+++ client/src/components/Sidebar/components/SliderBlockList/SliderBlockList.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import styles from './SliderBlockList.module.scss'
+
+const SliderBlockList: React.FC = function SliderBlockList() {
+  return (
+    <div className={styles.SliderBlockList}>
+
+      {/* <div className={styles.SidebarBlock__test_element}>
+        
+      </div> */}
+    </div>
+  )
+}
+
+export default SliderBlockList
---
+++ client/src/components/Sidebar/components/SliderBlockList/index.ts
@@ -0,0 +1,1 @@
+export { default as SliderBlockList } from './SliderBlockList'
