114165b55321d1711be2cb7de4bd94677e714258
alex.n.kior <alex.n.kior@gmail.com>
2023-04-27 16:23:08 +0300

    Add clickhouse

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClickhouseService } from './clickhouse/clickhouse.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly clickhouseService: ClickhouseService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    try {
+      const result = await this.clickhouseService.execute('SELECT 1');
+      return `Hello World! ClickHouse result: ${JSON.stringify(result)}`;
+    } catch (error) {
+      console.error('Error executing ClickHouse query:', error);
+      return 'Hello World! ClickHouse error.';
+    }
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClickhouseService } from './clickhouse/clickhouse.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ClickhouseService],
 })
 export class AppModule {}
