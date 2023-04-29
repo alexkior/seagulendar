8bc793ca64a78435aa4ac47c935ab130c8962057
alex.n.kior <alex.n.kior@gmail.com>
2023-04-29 20:08:32 +0300

    Dockersize db + server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Item } from './Item/item.entity';
-import { ItemController } from './Item/item.controller';
-import { ItemService } from './Item/item.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'seagulendar_admin',
-      password: 'seagull',
-      database: 'seagulendar_db',
-      entities: [Item],
-      synchronize: true,
-    }),
-  ],
-  controllers: [AppController, ItemController],
-  providers: [AppService, ItemService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
