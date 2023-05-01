5ca580a26769b5d8d7db373d3684eb2bddf2a62d
alex.n.kior <alex.n.kior@gmail.com>
2023-05-01 03:59:48 +0300

    Add common package json

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,29 +4,40 @@ import styles from './Header.module.scss'
 import { LoginButton } from '@telegram-auth/react'
 
 const Header: React.FC = function Header() {
+  const mockUser = {
+    auth_date: 1682895926,
+    first_name: 'Alexey',
+    hash: '898703291eb4b77d7600ca140f66a52389f5fe21199e82da7c4d2303ae17f4c6',
+    id: 222548982,
+    last_name: 'Kior',
+    photo_url:
+      'https://t.me/i/userpic/320/sZYMIpjHmSMoU8c79_VSG5vybC_7KAvYkH2ns7OAP3s.jpg',
+    username: 'alexki0r',
+  }
+
   return (
     <>
       <header className={styles.Header}>
-        {/* <motion.h2
+        <motion.button
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
           className={styles.Header__userName}
+          onClick={() => console.log(mockUser)}
         >
-          UserName
-        </motion.h2> */}
-        <LoginButton
+          Get User
+        </motion.button>
+        {/* <LoginButton
           botUsername="seagulendar_bot"
           onAuthCallback={(data) => {
             console.log(data)
-            
           }}
           // authCallbackUrl="http://localhost:3000/login"
           buttonSize="large" // "large" | "medium" | "small"
           cornerRadius={5} // 0 - 20
           showAvatar={true} // true | false
           lang="en"
-        />
+        /> */}
 
         <h1 className={styles.Header__projectName}>Seagulendar</h1>
       </header>
