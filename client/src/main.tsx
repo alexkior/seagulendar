ae8bb9213b84537029b957be7a1de27c4c23d7e8
alex.n.kior <alex.n.kior@gmail.com>
2023-05-06 01:08:58 +0300

    Implement new scss logic

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import {
   RouterProvider
 } from 'react-router-dom'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+
 const queryClient = new QueryClient()
+
 import { router } from './router'
+
 import './scss/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
