7a636c7d36ac07555037698dc454042c3acfbbbc
alex.n.kior <alex.n.kior@gmail.com>
2023-05-08 22:32:04 +0300

    Fix nginx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
-  server: {
-    host: '127.0.0.1',
-    port: 8080,
-    https: true
-  },
+  // server: {
+  //   host: '127.0.0.1',
+  //   port: 8080,
+  //   https: true
+  // },
 })
