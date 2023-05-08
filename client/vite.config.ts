dbf50cb55ee489ad1e5b57d27ab272b62bebb93f
alex.n.kior <alex.n.kior@gmail.com>
2023-05-09 00:10:18 +0300

    Fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    host: '127.0.0.1',
-    port: 8080,
-    https: true
-  },
+  plugins: [react()],
 })
