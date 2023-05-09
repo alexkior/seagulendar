64962285493a56999e6fe302ae1d70350ae2c5c8
alex.n.kior <alex.n.kior@gmail.com>
2023-05-09 20:56:11 +0300

    Fix vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,18 @@ export default defineConfig({
     react(),
     !isProduction && mkcert(),
   ].filter(Boolean),
+  build: {
+    // Enable cache-busting by including a content hash in the output file names
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        // This will generate file names like "index.12345678.js" and "index.abcdefgh.css"
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 8080,
